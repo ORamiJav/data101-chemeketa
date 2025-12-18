918eefa74efa1f26be9858ed25366cc3c6d08950
Peter Banwarth Carrillo <pbanwart@chemeketa.edu>
2025-12-18 22:13:58 +0000

    Final sync: Updated questions and manual test file adjustments for Lab 02

--- lab/lab02/tests/.ipynb_checkpoints/q11-checkpoint.py
+++ lab/lab02/tests/.ipynb_checkpoints/q11-checkpoint.py
@@ -1,1 +1,2 @@
-test = {'name': 'q11', 'points': None, 'suites': [{'cases': [{'code': '>>> new_year\n2022', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {'name': 'q11', 'points': None, 'suites': [{'cases': [{'code': '>>> new_year\n2026', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+OK_FORMAT = True
