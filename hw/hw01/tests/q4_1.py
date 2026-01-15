26d1a7ad945d8b57433a7409e714f33917192c79
Peter Banwarth Carrillo <pbanwart@chemeketa.edu>
2026-01-15 17:22:42 +0000

    Corrected test case q4_1 in hw01

--- hw/hw01/tests/q4_1.py
+++ hw/hw01/tests/q4_1.py
@@ -1,7 +1,7 @@
 test = {   'name': 'q4_1',
     'points': [0, 5],
     'suites': [   {   'cases': [   {'code': '>>> isinstance(biggest_change, (int, float))\nTrue', 'hidden': False, 'locked': False},
-                                   {'code': '>>> biggest_change == 216\nTrue', 'hidden': True, 'locked': False}],
+                                   {'code': '>>> biggest_change == 184\nTrue', 'hidden': True, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
